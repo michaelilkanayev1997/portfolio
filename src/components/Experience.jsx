b9ae8c0afeef24e8db3fe6196160d26dc4be4b08
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-17 01:48:14 +0300

    commit fix

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -48,7 +48,7 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="py-6">
+          <p className="md:py-6">
             These are some of the technologies I've worked with :
           </p>
         </div>
