7d4abeafe1e22e0a203bfe4b4777c4b4304b872f
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-17 03:15:45 +0300

    added ico + Nav fix + Portfolio fix

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -39,28 +39,25 @@ const Portfolio = () => {
       name="portfolio"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white ms:h-screen pb-20"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8 ">
+      <div className="max-w-screen-lg p-4 pt-20 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-0 sm:pb-2 ">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500 z-10">
             Portfolio
           </p>
           <p className="py-6 z-10">Check out some of my work :</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 z-10">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-12">
           {portfolios.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              />
+            <div
+              key={id}
+              className="shadow-md shadow-gray-600 rounded-lg duration-200 hover:scale-105 z-10"
+            >
+              <img src={src} alt="" className="rounded-md" />
+
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                <button className="font-semibold tracking-wide transform w-full px-6 py-1 m-1 sm:m-4 duration-200 hover:scale-105">
+                  Read More
                 </button>
               </div>
             </div>
