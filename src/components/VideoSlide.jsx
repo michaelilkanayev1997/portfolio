ea1ce1d9ecd19ef036c12146a89d74f3a6adcb49
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-18 17:21:24 +0300

    data added & fix for nav

--- src/components/VideoSlide.jsx
+++ src/components/VideoSlide.jsx
@@ -1,58 +1,40 @@
-import React, { useRef } from "react";
-import { Navigation, Pagination, Keyboard, A11y } from "swiper/modules";
+import React from "react";
+import { A11y, Keyboard, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css/keyboard";
-import "swiper/css/a11y";
 
 const VideoSlide = ({ videos }) => {
   return (
-    <>
-      <Swiper
-        modules={[Keyboard, Pagination, Navigation, A11y]}
-        navigation={true}
-        pagination={{ clickable: true }}
-        grabCursor={true}
-        spaceBetween={0}
-        slidesPerView={1}
-        keyboard={{
-          enabled: true,
-        }}
-        style={{
-          "--swiper-pagination-color": "#FFBA08",
-          "--swiper-pagination-bullet-inactive-color": "#999999",
-          "--swiper-pagination-bullet-size": "10px",
-        }}
-      >
-        {videos?.map((url, i) => (
-          <SwiperSlide key={i}>
-            <Video key={i} url={url} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
-  );
-};
-
-const Video = (props) => {
-  const url = props.url;
-
-  const iframeRef = useRef(null);
-
-  return (
-    <div
-      className="video"
-      style={{ padding: "40px", paddingTop: "0", paddingBottom: "0%" }}
+    <Swiper
+      modules={[Keyboard, Pagination, Navigation, A11y]}
+      navigation={true}
+      pagination={{ clickable: true }}
+      grabCursor={true}
+      spaceBetween={0}
+      slidesPerView={1}
+      keyboard={{
+        enabled: true,
+      }}
+      style={{
+        "--swiper-pagination-color": "#FFBA08",
+        "--swiper-pagination-bullet-inactive-color": "white",
+        "--swiper-pagination-bullet-size": "10px",
+        "--swiper-pagination-bullet-horizontal-gap": "15px",
+        "--swiper-pagination-bottom": "-7px",
+      }}
     >
-      <div className="video__title"></div>
-      <iframe
-        className="w-full h-auto sm:h-[315px] md:h-[420px] lg:h-[560px] xl:h-[640px]"
-        src={url}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
-    </div>
+      {[...videos].splice(0, 10).map((item, index) => (
+        <SwiperSlide key={index}>
+          <iframe
+            title="video"
+            width="100%"
+            className="w-full h-auto sm:h-[315px] md:h-[420px] lg:h-[560px]   xl:h-[576px] z-10"
+            src={item}
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
 
--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -1,109 +1,102 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import PicturesSlide from "../components/PicturesSlide";
+import VideoSlide from "../components/VideoSlide";
 
 const ProjectDetails = () => {
   const location = useLocation();
-  const project = location.state.project;
+  const {
+    download,
+    demo,
+    git,
+    introduction,
+    pictures,
+    secondText,
+    secondTitle,
+    thirdText,
+    thirdTitle,
+    videos,
+  } = location.state.project;
+  const { title } = location.state.title;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="w-full h-full bg-gradient-to-b  from-black  to-gray-800 text-white">
       <div className="w-full h-full max-w-screen-lg p-4 pt-24 sm:pt-0 mx-auto flex flex-col justify-center">
         <div className="text-xl md:mt-24 mt-0 mx-auto text-center">
-          <p className="text-4xl font-bold border-b-4 border-gray-500 inline-flex z-10">
-            Game-Of-Death
+          <p className="text-4xl md:text-5xl font-bold border-b-4 border-gray-500 inline-flex z-10">
+            {title}
           </p>
-          <p className="text-white-500 py-4 max-w-xl z-10 mx-auto text-center">
-            "Game of Death" stands not only as a personal project but also as a
-            testament to my journey of self-improvement as a software engineer.
-            This large and immersive third-person game was built in Unreal
-            Engine 5, with C++ and Blueprint scripting.
+          <p className="text-lg sm:text-xl text-white-500 py-4  z-10 mx-auto text-center">
+            {introduction}
           </p>
 
-          <p className="m-4 rounded-lg text-yellow-100 inline ">
-            Pictures from the game:
-          </p>
-
-          {project?.pictures?.length > 0 && (
-            <div className="max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-5xl mx-auto flex items-center z-10">
-              <PicturesSlide backdrops={project?.pictures} />
-            </div>
+          {pictures?.length > 0 && (
+            <>
+              <h2 className="text-yellow-100 text-2xl sm:text-3xl font-bold mt-4 mb-4 border-b-4 border-gray-500 inline-flex">
+                Images
+              </h2>
+              <div className="max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-5xl mx-auto flex items-center z-10">
+                <PicturesSlide backdrops={pictures} />
+              </div>
+            </>
           )}
 
           <p className="text-gray-500 py-4 max-w-2xl z-10 mx-auto text-center">
             Click on the images to view them more closely.
           </p>
 
-          <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
-            Crafting the Experience
-          </h2>
-          <p>
-            Drawing inspiration from Bruce Lee's "Game of Death," I embarked on
-            crafting an engaging gaming experience. The game features three
-            progressively challenging stages, each presenting unique bosses and
-            obstacles. Through rigorous learning and experimentation, I brought
-            these levels to life, enhancing my problem-solving skills in the
-            process.
-          </p>
+          {secondTitle && (
+            <>
+              <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
+                {secondTitle}
+              </h2>
+              <p className="text-lg sm:text-xl">{secondText}</p>
+            </>
+          )}
 
-          <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
-            Skill Development
-          </h2>
-          <p>
-            The project commenced as a self-taught endeavor. I dove into the
-            world of Unreal Engine, C++ programming, and Blueprint scripting.
-            Creating "Game of Death" served as an intense learning opportunity.
-            I mastered programming with C++ in Visual Studio, implemented
-            intricate logic through Blueprints, and fine-tuned graphical
-            elements. This multifaceted skill development equipped me with a
-            toolkit essential for tackling diverse development challenges.
-          </p>
+          {thirdTitle && (
+            <>
+              <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
+                {thirdTitle}
+              </h2>
+              <p className="text-lg sm:text-xl">{thirdText}</p>
+            </>
+          )}
 
-          <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
-            Building Unique Features
-          </h2>
-          <p>
-            The project boasts diverse features, including enemies with random
-            pickups, bosses with unique attacks, and character progression
-            through collectibles. I incorporated health and stamina systems and
-            coin-based upgrades, demonstrating my ability to merge creative
-            storytelling with complex mechanics.
-          </p>
+          {videos?.length > 0 && (
+            <>
+              <h2 className="text-yellow-100 text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
+                Videos
+              </h2>
 
-          <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
-            Conclusion
-          </h2>
-          <p>
-            "Game of Death" for me is more than a game; it's a narrative of
-            personal evolution. Through traversing the landscapes of C++
-            programming, software architecture, and creative problem-solving,
-            I've unlocked a newfound proficiency. As the project reaches its
-            conclusion, I'm left with not just a game but a skill set that
-            empowers me to create innovative solutions and bring imaginative
-            concepts to life in the realm of software development.
-          </p>
+              <div className="max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-5xl mx-auto flex items-center z-10">
+                <VideoSlide videos={videos} />
+              </div>
+            </>
+          )}
 
-          <h2 className="text-2xl sm:text-3xl font-bold mt-8 mb-4 border-b-4 border-gray-500 inline-flex">
-            Videos
-          </h2>
-          <iframe
-            title="video"
-            className="w-full h-auto sm:h-[315px] md:h-[420px] lg:h-[560px] xl:h-[576px] z-10"
-            src="https://www.youtube.com/embed/PMsAm4yWlOA"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-          <p className="mt-4">
-            You can download the game for free at the following link:{" "}
+          <div className="flex justify-center space-x-8 pt-8 pb-8">
             <a
-              href="https://drive.google.com/file/d/1c6KBDMeUnWDJ3G6rZSkvV6xHV5xjscsN/view"
+              href={download ? download : demo}
               target="_blank"
               rel="noopener noreferrer"
-              className="underline text-blue-500"
+              className="bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out w-1/3 sm:w-1/5"
             >
-              Download Game
+              {download ? "Download" : "Demo"}
             </a>
-          </p>
+            <a
+              href={git}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bg-gradient-to-r from-green-500 to-teal-500 hover:from-green-600 hover:to-teal-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out w-1/3 sm:w-1/5"
+            >
+              GitHub
+            </a>
+          </div>
         </div>
       </div>
     </div>
