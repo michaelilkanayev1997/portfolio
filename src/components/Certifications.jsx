6476e0a052506435b04376293ac7dacc5d9e8b1f
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-10-28 16:41:17 +0300

    Added Udemy React Native Course Certificate

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -6,6 +6,7 @@ import certificate2 from "../assets/certificates/certificate2.webp";
 import certificate3 from "../assets/certificates/certificate3.webp";
 import certificate4 from "../assets/certificates/certificate4.webp";
 import certificate5 from "../assets/certificates/certificate5.webp";
+import certificate6 from "../assets/certificates/certificate6.webp";
 
 const certifications = [
   {
@@ -21,16 +22,21 @@ const certifications = [
   {
     id: 3,
     img: certificate3,
-    src: "https://www.udemy.com/certificate/UC-2ba67ac7-fcbd-49fe-8628-8d54ff0037c1/",
+    src: "https://www.udemy.com/certificate/UC-33ff7171-63d3-4236-8028-33f09aa7a421/",
   },
   {
     id: 4,
     img: certificate4,
-    src: "https://www.udemy.com/certificate/UC-45c1f880-d0f4-47d7-ba80-6b72780ea5c6/",
+    src: "https://www.udemy.com/certificate/UC-2ba67ac7-fcbd-49fe-8628-8d54ff0037c1/",
   },
   {
     id: 5,
     img: certificate5,
+    src: "https://www.udemy.com/certificate/UC-45c1f880-d0f4-47d7-ba80-6b72780ea5c6/",
+  },
+  {
+    id: 6,
+    img: certificate6,
     src: "https://www.udemy.com/certificate/UC-f923fe11-2e24-4a11-bb2f-eb2499e08417/",
   },
 ];
