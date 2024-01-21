226ac9c2d5f9af7e666448a3d5c69e8b298f010d
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2024-01-21 23:24:12 +0200

    Added 2 Certificates From Udemy

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow, Pagination } from "swiper/modules";
+
 import certificate1 from "../assets/certificates/certificate1.webp";
 import certificate2 from "../assets/certificates/certificate2.webp";
 import certificate3 from "../assets/certificates/certificate3.webp";
 import certificate4 from "../assets/certificates/certificate4.webp";
 import certificate5 from "../assets/certificates/certificate5.webp";
 import certificate6 from "../assets/certificates/certificate6.webp";
+import certificate7 from "../assets/certificates/certificate7.webp";
+import certificate8 from "../assets/certificates/certificate8.webp";
+
 import { isMobile } from "../utils";
 
 const certifications = [
@@ -16,28 +20,38 @@ const certifications = [
     src: "https://www.udemy.com/certificate/UC-631fb29d-5cbb-4ca5-a8de-6745b4b57284/",
   },
   {
-    id: 4,
+    id: 2,
     img: certificate2,
+    src: "https://www.udemy.com/certificate/UC-0f2ecdca-6585-4f74-9d41-b5bd1888704f/",
+  },
+  {
+    id: 3,
+    img: certificate3,
+    src: "https://www.udemy.com/certificate/UC-79d75d78-d223-4d1f-96e3-51287eeee323/",
+  },
+  {
+    id: 4,
+    img: certificate4,
     src: "https://www.udemy.com/certificate/UC-0e33df7a-5d2f-4bc7-840d-c9411cf25042/",
   },
   {
     id: 5,
-    img: certificate3,
+    img: certificate5,
     src: "https://www.udemy.com/certificate/UC-33ff7171-63d3-4236-8028-33f09aa7a421/",
   },
   {
     id: 6,
-    img: certificate5,
+    img: certificate6,
     src: "https://www.udemy.com/certificate/UC-45c1f880-d0f4-47d7-ba80-6b72780ea5c6/",
   },
   {
     id: 7,
-    img: certificate6,
+    img: certificate7,
     src: "https://www.udemy.com/certificate/UC-f923fe11-2e24-4a11-bb2f-eb2499e08417/",
   },
   {
     id: 8,
-    img: certificate4,
+    img: certificate8,
     src: "https://www.udemy.com/certificate/UC-2ba67ac7-fcbd-49fe-8628-8d54ff0037c1/",
   },
 ];
