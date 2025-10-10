7ac850cfba638e1a84dbd841d3474865654017cb
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-10-10 19:27:33 +0300

    Remove Font Awesome dependency and its import from the project

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
