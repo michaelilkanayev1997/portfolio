28599cd337184dcb46142104b73046148ff5b9d7
michaelilkanayev1997 <michaelilkanayev@gmail.com>
2025-10-10 12:55:22 +0300

    refactor: Update particle options for improved performance and interactivity

--- src/data/particlesData.js
+++ src/data/particlesData.js
@@ -1,4 +1,5 @@
 export const particleOptions = {
+  fpsLimit: 60,
   particles: {
     number: {
       value: 40,
@@ -6,47 +7,24 @@ export const particleOptions = {
         enable: true,
         value_area: 800,
       },
+      limit: 100,
     },
     color: {
       value: "#ffffff",
     },
     shape: {
       type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
     },
     opacity: {
-      value: 1,
+      value: 0.5,
       random: true,
-      anim: {
-        enable: true,
-        speed: 1,
-        opacity_min: 0,
-        sync: false,
-      },
     },
     size: {
       value: 3,
       random: true,
-      anim: {
-        enable: false,
-        speed: 4,
-        size_min: 0.3,
-        sync: false,
-      },
     },
     line_linked: {
-      enable: false,
+      enable: true,
       distance: 150,
       color: "#ffffff",
       opacity: 0.4,
@@ -60,11 +38,6 @@ export const particleOptions = {
       straight: false,
       out_mode: "out",
       bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 600,
-      },
     },
   },
   interactivity: {
@@ -72,37 +45,23 @@ export const particleOptions = {
     events: {
       onhover: {
         enable: true,
-        mode: "bubble",
+        mode: "grab",
       },
       onclick: {
         enable: true,
-        mode: "repulse",
+        mode: "push",
       },
       resize: true,
     },
     modes: {
       grab: {
-        distance: 400,
+        distance: 200,
         line_linked: {
           opacity: 1,
         },
       },
-      bubble: {
-        distance: 250,
-        size: 0,
-        duration: 2,
-        opacity: 0,
-        speed: 3,
-      },
-      repulse: {
-        distance: 400,
-        duration: 0.4,
-      },
       push: {
         particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
       },
     },
   },
